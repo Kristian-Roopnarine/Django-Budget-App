fe046c496a82bea817b37da8b127f3ce521d8470
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-10-11 13:36:50 -0400

    Checked the models to ensure they worked with Djangos API.

--- budget_project/budget_app/models.py
+++ budget_project/budget_app/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 class AccountInfo(models.Model):
     username = models.CharField(max_length=20)
-    password =models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
 
 class BudgetInfo(models.Model):
     user_budget = models.IntegerField()
     expenses = models.IntegerField()
-    category = models.CharField(max_length=10)
     user = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
