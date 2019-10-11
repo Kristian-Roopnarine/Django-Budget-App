3ca2d7ff6f40b362a4c565c073b1c2e76bd534d8
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-10-11 12:34:08 -0400

    Created migrations.

--- budget_project/budget_app/models.py
+++ budget_project/budget_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class AccountInfo(models.Model):
-    username_ = models.CharField(max=20)
-    password_=models.CharField(max=20)
+    username = models.CharField(max_length=20)
+    password =models.CharField(max_length=20)
 
 class BudgetInfo(models.Model):
     user_budget = models.IntegerField()
     expenses = models.IntegerField()
-    category = models.CharField(max=10)
+    category = models.CharField(max_length=10)
