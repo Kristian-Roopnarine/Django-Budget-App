86f385df6cc3199683adb53a9b2b12b19381321f
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-10-11 12:50:36 -0400

    Added many to one relation for users and budget info.

--- budget_project/budget_app/models.py
+++ budget_project/budget_app/models.py
@@ -10,3 +10,4 @@ class BudgetInfo(models.Model):
     user_budget = models.IntegerField()
     expenses = models.IntegerField()
     category = models.CharField(max_length=10)
+    user = models.ForeignKey(AccountInfo, on_delete=models.CASCADE)
