45285e082993b33b52d28ba6157d339bb8b6d18e
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-10-11 00:44:29 -0400

    Created models for account info and budget

--- budget_project/budget_app/models.py
+++ budget_project/budget_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class AccountInfo(models.Model):
+    username_ = models.CharField(max=20)
+    password_=models.CharField(max=20)
+
+class BudgetInfo(models.Model):
+    user_budget = models.IntegerField()
+    expenses = models.IntegerField()
+    category = models.CharField(max=10)
